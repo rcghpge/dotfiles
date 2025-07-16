69a12211a3b906b3af4e5e55958c3740aae78a01
Robert Cocker <rcocker87@gmail.com>
2025-07-16 04:24:46 -0500

    wsl-bash jupyter on chrome autolaunch setup

---
+++ scripts/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+# Update package index
+sudo apt update
+
+# Upgrade existing packages
+sudo apt upgrade -y
+
+# Install packages
+sudo apt install -y \
+  git \
+  curl \
+  neovim \
+  build-essential \
+  python3-pip \
+  wslu \
+  xdg-utils \
+  jupyter notebook \
+  # add more here...
+
+# Optional: cleanup
+sudo apt clean
+sudo apt autoclean
+sudo apt autoremove -y
+
